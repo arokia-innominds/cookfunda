9ae93916930b0a9996f9958e165435bd4c412e94
arokia-innominds <aalphonseraj@innominds.com>
2016-01-07 17:40:50 +0530

    Ingradients pic added

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,3 @@
 class Ingredient < ActiveRecord::Base
-  has_attached_file :pic, :styles => 
-           { :medium => "300x300>", small: "100x100>", :thumb => "50x50>" }
+  has_attached_file :pic, :styles => { :medium => "300x300>", small: "100x100>", :thumb => "50x50>" }, :default_style => :normal
 end
