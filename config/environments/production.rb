fb0faff9bcead87fd5f1b9dd1a9aec1d1f992628
arokia-innominds <aalphonseraj@innominds.com>
2016-01-07 16:07:31 +0530

    production setup for mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
@@ -79,14 +79,15 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               => "gmail.com",
-    :user_name            => "#{ENV['GMAIL_USER']}",
+    :user_name            => "cookfunda@gmail.com",
     :password             => "#{ENV['GMAIL_PASS']}",
-    :authentication       => :plain,
+    :authentication       => :login,
     :enable_starttls_auto => true
   }
 end
