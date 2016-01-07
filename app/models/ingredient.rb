ad3c22d5e063784695a43ccf079e9f0ba4a7b8e3
arokia-innominds <aalphonseraj@innominds.com>
2016-01-07 17:29:08 +0530

    Ingradients image size modified

--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,4 +1,4 @@
 class Ingredient < ActiveRecord::Base
   has_attached_file :attach, :styles => 
-           { :medium => "300x300>", :thumb => "100x100>" }
+           { :medium => "300x300>", small: "100x100>", :thumb => "50x50>" }
 end
